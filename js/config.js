5ac5253b67df536f4009055b6f887186071b643b
Meathill <meathill@gmail.com>
2013-05-28 00:28:52 +0800

    基本完成程序搭建和内容整理
    Signed-off-by: Meathill <meathill@gmail.com>

---
+++ js/config.js
@@ -0,0 +1,342 @@
+var CONFIG = [
+  {
+    'name': '神庙逃亡',
+    'description': '这是一款激动人心的跑酷游戏，现在已经成为了跑酷游戏中的经典。在游戏中你需要翻过古庙围墙，爬上悬崖峭壁，转弯、跳跃，躲过各种障碍物。跑得越远，得分越高。',
+    'options': [
+      {
+        'icon': '0-0.jpg',
+        'isRight': true
+      },
+      {
+        'icon': '0-1.jpg',
+        'title': '失落神庙跑酷',
+        'isRight': false
+      },
+      {
+        'icon': '0-2.jpg',
+        'title': '逃亡猪',
+        'isRight': false
+      },
+      {
+        'icon': '0-3.jpg',
+        'title': '荒庙逃亡',
+        'isRight': false
+      }
+    ]
+  },
+  {
+    'name': '捣蛋猪',
+    'description': '它是《愤怒的小鸟》的续作，仍然是一款物理益智游戏，但坏猪头翻身做上了主人，自制交通工具适应不同的路线，偷走小鸟们的蛋。',
+    'options': [
+      {
+        'icon': '1-0.jpg',
+        'isRight': true
+      },
+      {
+        'icon': '1-1.jpg',
+        'title': '捣蛋猪',
+        'isRight': false
+      },
+      {
+        'icon': '1-2.jpg',
+        'title': '愤怒的捣蛋猪之丛林有情猪',
+        'isRight': false
+      },
+      {
+        'icon': '1-3.jpg',
+        'title': '捣蛋飞天猪',
+        'isRight': false
+      },
+      {
+        'icon': '1-4.jpg',
+        'title': '捣蛋飞天猪2',
+        'isRight': false
+      },
+      {
+        'icon': '1-5.jpg',
+        'title': '捣蛋猪经典版',
+        'isRight': false
+      },
+      {
+        'icon': '1-6.jpg',
+        'title': '无聊的捣蛋猪',
+        'isRight': false
+      }
+    ]
+  },
+  {
+    'name': '地铁跑酷',
+    'description': '这是一款带有美式街头风格的跑酷游戏，采用3D立体视角，在充满动感和色彩的画面中奔跑、跳跃和翻滚。',
+    'options': [
+      {
+        'icon': '2-0.jpg',
+        'isRight': true
+      },
+      {
+        'icon': '2-1.jpg',
+        'title': '地铁跑酷',
+        'isRight': false
+      },
+      {
+        'icon': '2-2.jpg',
+        'title': '地铁冲浪者免费',
+        'isRight': false
+      },
+      {
+        'icon': '2-3.jpg',
+        'title': '地铁跑酷 【全新版】',
+        'isRight': false
+      }
+    ]
+  },
+  {
+    'name': '愤怒的小鸟',
+    'description': '家喻户晓的益智游戏，为了报复偷走鸟蛋的绿色坏猪头，小鸟化身武器去攻击坏猪头的堡垒，将猪头全部砸中便可过关。',
+    'options': [
+      {
+        'icon': '3-0.jpg',
+        'isRight': true
+      },
+      {
+        'icon': '3-1.jpg',
+        'title': '愤怒的小鸟翻翻乐',
+        'isRight': false
+      },
+      {
+        'icon': '3-2.jpg',
+        'title': '愤怒的小鸟(魔方版)',
+        'isRight': false
+      },
+      {
+        'icon': '3-3.jpg',
+        'title': '五子连珠（愤怒的小鸟）',
+        'isRight': false
+      },
+      {
+        'icon': '3-4.jpg',
+        'title': '愤怒的小鸟',
+        'isRight': false
+      },
+      {
+        'icon': '3-5.jpg',
+        'title': '愤怒的小鸟连连看',
+        'isRight': false
+      },
+      {
+        'icon': '3-6.jpg',
+        'title': '愤怒的小鸟感应版',
+        'isRight': false
+      },
+      {
+        'icon': '3-7.jpg',
+        'title': '愤怒的小鸟营救伙伴',
+        'isRight': false
+      },
+      {
+        'icon': '3-8.jpg',
+        'title': 'Angry Birds',
+        'isRight': false
+      },
+      {
+        'icon': '3-9.jpg',
+        'title': '愤怒的小鸟对对碰',
+        'isRight': false
+      }
+    ]
+  },
+  {
+    'name': '割绳子',
+    'description': '一款休闲益智游戏，小怪物OmNom令人过目不忘，深得人心。游戏中我们要想方设法剪断绳子，让糖果落入OmNom的口中。可爱的画面，愉快的音效以及创意的操作是这款游戏的主要特色。',
+    'options': [
+      {
+        'icon': '4-0.jpg',
+        'isRight': true
+      },
+      {
+        'icon': '4-1.jpg',
+        'title': '割绳子',
+        'isRight': false
+      }
+    ]
+  },
+  {
+    'name': '猴子也疯狂',
+    'description': '《猴子也疯狂》既可以单机也可以网络对战，它借鉴了愤怒的小鸟的物理投掷游戏模式，不过有非常多的创新点，不再是插在地上的弹弓了，而是许多动物吊在树上，会摇晃，增加了游戏的可玩性。',
+    'options': [
+      {
+        'icon': '5-0.jpg',
+        'isRight': true
+      },
+      {
+        'icon': '5-1.jpg',
+        'title': '猴子也疯狂',
+        'isRight': false
+      },
+      {
+        'icon': '5-2.jpg',
+        'title': '疯狂的猴子',
+        'isRight': false
+      }
+    ]
+  },
+  {
+    'name': '水果忍者',
+    'description': '不用多说，没有人不知道《水果忍者》，令人兴奋的手指挥舞以及爽快、逼真的音效让人对它爱不释手。',
+    'options': [
+      {
+        'icon': '6-0.jpg',
+        'isRight': true
+      },
+      {
+        'icon': '6-1.jpg',
+        'title': '水果忍者疯狂版',
+        'isRight': false
+      },
+      {
+        'icon': '6-2.jpg',
+        'title': '水果忍者免费版2',
+        'isRight': false
+      },
+      {
+        'icon': '6-3.jpg',
+        'title': '快刀切水果',
+        'isRight': false
+      },
+      {
+        'icon': '6-4.jpg',
+        'title': '水果忍者(竹林版)',
+        'isRight': false
+      },
+      {
+        'icon': '6-5.jpg',
+        'title': '忍者切水果3D',
+        'isRight': false
+      },
+      {
+        'icon': '6-6.jpg',
+        'title': '水果忍者Fans',
+        'isRight': false
+      },
+      {
+        'icon': '6-7.jpg',
+        'title': '水果忍者',
+        'isRight': false
+      },
+      {
+        'icon': '6-8.jpg',
+        'title': '切水果',
+        'isRight': false
+      },
+      {
+        'icon': '6-9.jpg',
+        'title': '菜刀切水果',
+        'isRight': false
+      }
+    ]
+  },
+  {
+    'name': '会说话的汤姆猫',
+    'description': '宠物类游戏，是Outfit7公司“会说话”(Talking Friend)系列应用的第一款游戏，上市后广受世界范围内用户的欢迎。汤姆猫不是用来宠爱，而是用来“学舌”和取乐，不用担心它会生气，反而是乐在其中。',
+    'options': [
+      {
+        'icon': '7-0.jpg',
+        'isRight': true
+      },
+      {
+        'icon': '7-1.jpg',
+        'title': '超级可爱小汤姆猫',
+        'isRight': false
+      },
+      {
+        'icon': '7-2.jpg',
+        'title': '会说话的汤姆猫3',
+        'isRight': false
+      },
+      {
+        'icon': '7-3.jpg',
+        'title': '会说话的汤姆猫3',
+        'isRight': false
+      },
+      {
+        'icon': '7-4.jpg',
+        'title': '会说话的汤姆猫4',
+        'isRight': false
+      }
+    ]
+  },
+  {
+    'name': '涂鸦跳跃',
+    'description': '一款富有趣味的游戏，在游戏中玩家要让涂鸦弹簧小怪物不停地往上跳跃，在跳跃中要小心破碎的平台、移动的蓝色平台、黑洞、不明飞行物和坏人，但也有快速上升的火箭和竹蜻蜓等好玩的道具。该游戏一反常态采用了涂鸦手绘风格，没想到获得了空前的成功。',
+    'options': [
+      {
+        'icon': '8-0.jpg',
+        'isRight': true
+      },
+      {
+        'icon': '8-1.jpg',
+        'title': '涂鸦跳跃',
+        'isRight': false
+      },
+      {
+        'icon': '8-2.jpg',
+        'title': '涂鸦跳跃',
+        'isRight': false
+      },
+      {
+        'icon': '8-3.jpg',
+        'title': '涂鸦跳跳',
+        'isRight': false
+      },
+      {
+        'icon': '8-4.jpg',
+        'title': '涂鸦跳跃春节版',
+        'isRight': false
+      },
+      {
+        'icon': '8-5.jpg',
+        'title': '涂鸦跳跃体感版',
+        'isRight': false
+      },
+      {
+        'icon': '8-6.jpg',
+        'title': '涂鸦跳跃上帝版',
+        'isRight': false
+      }
+    ]
+  },
+  {
+    'name': '鳄鱼小顽皮爱洗澡',
+    'description': '一个款简单有趣而又充满挑战的游戏，游戏中玩家要将水从泥土层引入小顽皮的浴。游戏中绝对创新的玩法，缸栩栩如生的水流，以及风趣幽默的效果等因素，让它成为令迪士尼最骄傲的手机游戏作品。',
+    'options': [
+      {
+        'icon': '9-0.jpg',
+        'isRight': true
+      },
+      {
+        'icon': '9-1.jpg',
+        'title': '小顽皮爱洗澡的玩具篇',
+        'isRight': false
+      },
+      {
+        'icon': '9-2.jpg',
+        'title': '小鳄鱼爱洗澡小泽龙蛇',
+        'isRight': false
+      },
+      {
+        'icon': '9-3.jpg',
+        'title': '爱洗澡的鳄鱼连连看',
+        'isRight': false
+      },
+      {
+        'icon': '9-4.jpg',
+        'title': '鳄鱼消消乐',
+        'isRight': false
+      },
+      {
+        'icon': '9-5.jpg',
+        'title': '小鳄鱼爱洗澡',
+        'isRight': false
+      }
+    ]
+  }
+];
--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
+$(function () {
+  'use strict';
+  function createTemplate(id) {
+    var source = $('#' + id).find('script').html();
+    return Handlebars.compile(source)
+  }
+  function showStage(index) {
+    if (index >= CONFIG.length) {
+      showFinalPopup();
+      $('#options').off('click');
+      return;
+    }
+    var stageData = CONFIG[index];
+    stageData.index = hanzi[index];
+    $('#question').text(outputQuestion(stageData));
+    $('#options').html(outputOptions(stageData));
+  }
+  function showRightPopup() {
 
+  }
+  function showWrongPopup() {
+
+  }
+  function showFinalPopup() {
+
+  }
+
+  var stage = 0,
+      right = 0,
+      hanzi = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十'],
+      outputQuestion = createTemplate('template'),
+      outputOptions = createTemplate('options'),
+      outputPopup = createTemplate('popup');
+  $('#template, #options').empty();
+
+  // events
+  $('#options').on('click', 'li', function (event) {
+    if (event.currentTarget.className === 'bingo') {
+      right += 1;
+      showRightPopup();
+    } else {
+      showWrongPopup();
+    }
+    stage += 1;
+    showStage(stage);
+  });
+
+  // statr
+  showStage(stage);
+});
